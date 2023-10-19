6a9c1721db62914c6809ad524deb6fb02bbaeb77
Omar Aseer <omar.aseer@sedco.co>
2023-10-19 09:48:28 +0300

    Added global 404 page to app and added user manual

---
+++ BankConfigurationPortal/BankConfigurationPortal.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace BankConfigurationPortal.Web.Controllers {
+    public class ErrorsController : Controller {
+        public ActionResult NotFound() {
+            return View("NotFound");
+        }
+    }
+}
