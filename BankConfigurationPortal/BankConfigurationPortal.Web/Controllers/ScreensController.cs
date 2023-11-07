56acd703705a6cc63aca76d4b3be441dea99e4c4
Omar Aseer <omar.aseer@sedco.co>
2023-11-07 14:56:30 +0300

    Separated API to 3 method overloads to handle erroneous inputs

--- BankConfigurationPortal/BankConfigurationPortal.Web/Controllers/ScreensController.cs
+++ BankConfigurationPortal/BankConfigurationPortal.Web/Controllers/ScreensController.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Web.Http;
 
 namespace BankConfigurationPortal.Web.Controllers {
+    [ApiAuthorization]
     public class ScreensController : ApiController {
         private readonly IScreenData db;
 
@@ -17,77 +18,98 @@ namespace BankConfigurationPortal.Web.Controllers {
             this.db = db;
         }
 
-        [ApiAuthorization]
-        public IHttpActionResult Get(int branchId = 0, int counterId = 0) {
+        public IHttpActionResult Get() {
             try {
-                string bankName = Encoding.UTF8.GetString(Convert.FromBase64String(Request.Headers.FirstOrDefault(h => h.Key.Equals(AuthenticationConstants.AUTHORIZATION_HEADER_NAME)).Value.First())).Split(':')[0];
+                string bankName = GetBankNameFromRequest();
 
                 var screen = db.GetActiveScreen(bankName);
-                if (screen.ScreenId == 0) {
-                    return NotFound();
-                }
+                if (screen.ScreenId == 0) return NotFound();
 
-                IEnumerable<TicketingButton> buttons;
-
-                if (branchId == 0 && counterId == 0) {
-                    buttons = db.GetButtons(bankName, screen.ScreenId);
-                }
-                else if (branchId != 0 && counterId == 0) {
-                    if (!db.CheckIfBranchExists(bankName, branchId)) {
-                        return NotFound();
-                    }
-                    
-                    buttons = db.GetButtons(bankName, screen.ScreenId, branchId);
-                }
-                else if (branchId != 0 && counterId != 0) {
-                    if (!db.CheckIfCounterExists(bankName, branchId, counterId)) {
-                        return NotFound();
-                    }
-
-                    buttons = db.GetButtons(bankName, screen.ScreenId, branchId, counterId);
-                }
-                else {
-                    return BadRequest("Missing branch ID.");
-                }
-
-                ScreenModel ret = new ScreenModel() {
-                    BankName = screen.BankName,
-                    ScreenId = screen.ScreenId,
-                    ScreenTitle = screen.ScreenTitle,
-                    ButtonCount = buttons.Count(),
-                    Buttons = new ButtonModel[buttons.Count()],
-                };
-
-                int i = 0;
-                foreach (var button in buttons) {
-                    int serviceId = 0;
-                    string messageEn = string.Empty, messageAr = string.Empty;
-                    if (button is IssueTicketButton issueTicketButton) {
-                        serviceId = issueTicketButton.ServiceId;
-                    }
-                    if (button is ShowMessageButton showMessageButton) {
-                        messageEn = showMessageButton.MessageEn;
-                        messageAr = showMessageButton.MessageAr;
-                    }
-
-                    ret.Buttons[i] = new ButtonModel {
-                        ButtonId = button.ButtonId,
-                        Type = Enum.GetName(typeof(ButtonsConstants.Types), button.Type),
-                        NameEn = button.NameEn,
-                        NameAr = button.NameAr,
-                        ServiceId = serviceId,
-                        MessageEn = messageEn,
-                        MessageAr = messageAr
-                    };
-                    ++i;
-                }
-
-                return Ok(ret);
+                IEnumerable<TicketingButton> buttons = db.GetButtons(bankName, screen.ScreenId);
+                return Ok(GetScreenModel(screen, buttons));
             }
             catch (Exception ex) {
                 ExceptionHelper.HandleGeneralException(ex);
                 return InternalServerError(new Exception("An unexpected error has occurred."));
             }
+        }
+
+        public IHttpActionResult Get(int? branchId) {
+            try {
+                string bankName = GetBankNameFromRequest();
+
+                if (branchId is null) return BadRequest();
+                if (!db.CheckIfBranchExists(bankName, branchId.Value)) return NotFound();
+
+                var screen = db.GetActiveScreen(bankName);
+                if (screen.ScreenId == 0) return NotFound();
+
+                IEnumerable<TicketingButton> buttons = db.GetButtons(bankName, screen.ScreenId, branchId.Value);
+                return Ok(GetScreenModel(screen, buttons));
+            }
+            catch (Exception ex) {
+                ExceptionHelper.HandleGeneralException(ex);
+                return InternalServerError(new Exception("An unexpected error has occurred."));
+            }
+        }
+
+        public IHttpActionResult Get(int? branchId, int? counterId) {
+            try {
+                string bankName = GetBankNameFromRequest();
+
+                if (branchId is null || counterId is null) return BadRequest();
+                if (!db.CheckIfCounterExists(bankName, branchId.Value, counterId.Value)) return NotFound();
+
+                var screen = db.GetActiveScreen(bankName);
+                if (screen.ScreenId == 0) return NotFound();
+
+                IEnumerable<TicketingButton> buttons = db.GetButtons(bankName, screen.ScreenId, branchId.Value, counterId.Value);
+                return Ok(GetScreenModel(screen, buttons));
+            }
+            catch (Exception ex) {
+                ExceptionHelper.HandleGeneralException(ex);
+                return InternalServerError(new Exception("An unexpected error has occurred."));
+            }
+        }
+
+        private ScreenModel GetScreenModel(TicketingScreen screen, IEnumerable<TicketingButton> buttons) {
+            ScreenModel ret = new ScreenModel() {
+                BankName = screen.BankName,
+                ScreenId = screen.ScreenId,
+                ScreenTitle = screen.ScreenTitle,
+                ButtonCount = buttons.Count(),
+                Buttons = new ButtonModel[buttons.Count()],
+            };
+
+            int i = 0;
+            foreach (var button in buttons) {
+                int serviceId = 0;
+                string messageEn = string.Empty, messageAr = string.Empty;
+                if (button is IssueTicketButton issueTicketButton) {
+                    serviceId = issueTicketButton.ServiceId;
+                }
+                if (button is ShowMessageButton showMessageButton) {
+                    messageEn = showMessageButton.MessageEn;
+                    messageAr = showMessageButton.MessageAr;
+                }
+
+                ret.Buttons[i] = new ButtonModel {
+                    ButtonId = button.ButtonId,
+                    Type = Enum.GetName(typeof(ButtonsConstants.Types), button.Type),
+                    NameEn = button.NameEn,
+                    NameAr = button.NameAr,
+                    ServiceId = serviceId,
+                    MessageEn = messageEn,
+                    MessageAr = messageAr
+                };
+                ++i;
+            }
+
+            return ret;
+        }
+
+        private string GetBankNameFromRequest() {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(Request.Headers.FirstOrDefault(h => h.Key.Equals(AuthenticationConstants.AUTHORIZATION_HEADER_NAME)).Value.First())).Split(':')[0];
         }
 
         private class ScreenModel {
